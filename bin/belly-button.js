c1f407210e566d0f01950cb2c0b742a73ddaec27
Adam Bretz <arbretz@gmail.com>
2015-10-19 14:37:29 -0400

    Styled error message output. Corrected space around () rule. Closes #3.

--- bin/belly-button.js
+++ bin/belly-button.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-require('../lib/cli').run(process.argv, function(err, output, code) {
+require('../lib/cli').run(process.argv, function (err, output, code) {
   if (err) {
     console.error(err.message);
     process.exit(1);
